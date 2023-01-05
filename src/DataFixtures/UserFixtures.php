7a10c6e956c91307f85826b270948f5d2a31b2f3
CRaph0705 <raphaelcochrane@hotmail.fr>
2023-01-05 11:00:46 +0100

    load first fixture user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword('$2y$13$p85RfnPn/6dA5ZDHVNES.eO3Euhw/kvU8ZLieLhcpa1xy8fbG/2RK');
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
